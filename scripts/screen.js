53bd27df2fd615bc4b5a949dac15e6628f2eaab8
langel <knoxburry@gmail.com>
2023-09-25 00:08:38 -0400

    fps control (set to 48fps for now)

--- scripts/screen.js
+++ scripts/screen.js
@@ -3,23 +3,21 @@ const s_ctx = document.getElementById('screen').getContext('2d');
 const s_width = 256;
 const s_height = 224;
 const s_rat = s_width / s_height;
+const s_fps = 48;
+const s_interval = 1000 / s_fps;
+let s_frame_count = 0;
+let s_start, s_now, s_then, s_elapsed;
 let sw, sh;
 let sprite;
+let sy = 10;
 
 window.addEventListener('load', () => {
 	s_ctx.canvas.width = s_width;
 	s_ctx.canvas.height = s_height;
+	s_then = s_start = window.performance.now();
+	s_frame();
 	sprite = new Image();
 	sprite.src = "graphics/sprites_enemies_hipu.png";
-	sprite.onload = function() {
-		s_resize();
-		s_render();
-	}
-});
-
-window.addEventListener('resize', () => {
-	s_resize();
-	s_render();
 });
 
 function s_resize() {
@@ -42,8 +40,17 @@ function s_resize() {
 	s_ctx.canvas.style.height = sh;
 }
 
-function s_render() {
-	s_ctx.fillStyle = '#ccc';
-	s_ctx.fillRect(0, 0, sw, sh);
-	s_ctx.drawImage(sprite, 64, 64);
+function s_frame() {
+	requestAnimationFrame(s_frame);
+	s_now = window.performance.now();
+	s_elapsed = s_now - s_then;
+	if (s_elapsed > s_interval) {
+		s_then = s_now - (s_elapsed % s_interval);
+		s_frame_count++;
+		s_resize();
+		sy = (sy + 1) % 200;
+		s_ctx.fillStyle = '#305182';
+		s_ctx.fillRect(0, 0, sw, sh);
+		s_ctx.drawImage(sprite, 64, sy);
+	}
 }
